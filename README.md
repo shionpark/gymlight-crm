871bde78a106eb7b83b18863edef692ea80d43a4
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-04-25 15:27:17 +0900

    Routers
    globalRouter, userRouter, centerRouter

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# gymlight-crm
-customer relationship management system
+# Gym-Light CRM
+
+Gym-light Fitness CRM(Customer Relationship Management)
+
+## Router
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/centers -> See Centers
+/centers/accounting -> See Accounting
+/centers/:id -> See Center
+/centers/:id/attendance -> See Attendance
+/centers/:id/lockers -> See Locker
+/centers/:id/sales -> See Sales
--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const centerRouter = express.Router();
+const handleSalesCenter = (req, res) => res.send("Sales");
+centerRouter.get("/:id/sales", handleSalesCenter);
+
+app.use("/", globalRouter);
+app.use("/centers", centerRouter);
+app.use("/users", userRouter);
+
 const PORT = 4001;
 
 const handleListening = () =>
