3dfe9e26b2762468f5db5875917f097196e93fe0
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-04-26 23:37:25 +0900

    Connecting to Mongo

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -20,6 +21,6 @@ app.use("/users", userRouter);
 const PORT = 4001;
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
